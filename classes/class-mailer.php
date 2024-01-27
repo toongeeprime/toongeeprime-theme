f3e109070dd46450e5e24238b8643a793f23e195
Tunji Makanjuola <39152476+toongeeprime@users.noreply.github.com>
2024-01-27 22:43:05 +0100

    Update class-mailer.php

--- classes/class-mailer.php
+++ classes/class-mailer.php
@@ -18,14 +18,16 @@ class Prime2gMailer {
 	public $sender_name;
 	public $sender_email;
 	public $default_css	=	'yes';
-	public $css		=	'';
-	public $headers	=	'';		// string|string[]
-	public $subject	=	'MAILER CLASS TEST';
+	public $css			=	'';
+	public $headers		=	'';	// string|string[]
+	public $subject		=	'MAILER CLASS TEST';
 	public $recipients;		// string|string[]
 	public $mail_content	=	'<h3>ToongeePrime Theme Mailer Test</h3><p>Testing theme\'s mailer class.</p>';
 	public $attachments	=	[];	// string|string[]
 	public $closing_remark;
 
+// $headers[]	=	'Reply-To: Person Name <person.name@akawey.com>';
+// $headers[]	=	'Cc: John <john@akawey.com>';
 
 	/**
 	 *	Defaults
@@ -38,7 +40,7 @@ class Prime2gMailer {
 		'domain'	=>	prime2g_get_site_domain(),
 		'sender_name'	=>	$siteName,
 		'sender_email'	=>	get_option( 'admin_email' ),
-		'closing_remark'=>	'<p>' . __( 'Kind Regards', PRIME2G_TEXTDOM ) .',</p><p>' . esc_html( $siteName ) . '.</p>',
+		'closing_remark'=>	'<p>'. __( 'Kind Regards', PRIME2G_TEXTDOM ) .',</p><p>'. esc_html( $siteName ) .'.</p>',
 		'recipients'	=>	$this->admin_emails( $domain )
 	];
 	}
@@ -84,13 +86,11 @@ class Prime2gMailer {
 	public function send() {
 	add_filter( 'wp_mail_content_type', function() { return $this->content_type; } );
 
-	//	Set "From:" headers (These get overridden by theme SMTP)
+	//	Set "From:" headers (These get overridden by phpmailer_init action)
 	add_filter( 'wp_mail_from_name', function() { return $this->sender_name; } );
 	add_filter( 'wp_mail_from', function() { return $this->sender_email; } );
 
 	return wp_mail( $this->recipients, $this->subject, $this->mail_template(), $this->headers, $this->attachments );
-// $headers[]	=	'Reply-To: Person Name <person.name@akawey.com>';
-// $headers[]	=	'Cc: John <john@akawey.com>';
 	}
 
 
@@ -98,8 +98,8 @@ class Prime2gMailer {
 	 *	Admin Emails
 	 */
 	public function admin_emails( string $domain = '' ) {
-		$smptemail	=	get_theme_mod( 'prime2g_smtp_username' ) ?: ''; # is email
 		$adminEmail	=	get_option( 'admin_email' );
+		$smptemail	=	get_theme_mod( 'prime2g_smtp_username' ) ?: ''; # is email
 		$admin_alt	=	$domain ? 'admin@' . $domain : '';
 	return array( $adminEmail, $smptemail, $admin_alt );
 	}
@@ -133,9 +133,9 @@ if ( $this->default_css === 'yes' ) {
 $css	.=	'#mail_details #mheader img{max-height:100px;width:auto;}
 #mail_details #mheader{padding:10px;text-align:center;}
 #mail_details #mheader p,#mail_details #intro{border:0;display:block;}
-#mail_page{background:#444;padding:40px;}
+#mail_page{background:#555;padding:30px;}
 #in_mail_page{background:#fff;color:#303030;padding:25px;width:90%;border-radius:5px;
-max-width:600px;margin:auto;box-shadow:0 20px 25px 5px rgba(0,0,0,0.5);}
+max-width:600px;margin:auto;box-shadow:0 10px 20px 5px rgba(0,0,0,0.5);}
 #mail_details p{padding:15px 0;margin:0;}
 #mail_details #custom_b_note{padding:30px 0;}
 #mail_details p span{font-weight:600;display:inline-block;min-width:220px;}
